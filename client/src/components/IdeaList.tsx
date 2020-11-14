dc005326386b7a5f213df72aafffe54eb27e7303
Josh Ellis <josh@imjoshellis.com>
2020-11-14 12:18:08 -0600

    Add styles

--- client/src/components/IdeaList.tsx
+++ client/src/components/IdeaList.tsx
@@ -6,9 +6,11 @@ interface IdeaListProps {}
 export const IdeaList: React.FC<IdeaListProps> = () => {
   const [ideas] = useIdeas()
   return (
-    <div data-testid='idea-list'>
+    <div data-testid='idea-list' className='flex flex-col text-lg'>
       {ideas.map((idea, idx) => (
-        <div key={idx}>{idea}</div>
+        <div className='mb-2' key={idx}>
+          {idea}
+        </div>
       ))}
     </div>
   )
