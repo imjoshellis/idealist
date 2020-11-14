c8b35b0320c003f2afb0a9727b3d3969e3c382e9
Josh Ellis <josh@imjoshellis.com>
2020-11-14 12:41:38 -0600

    Add link to add more to list

--- client/src/pages/list.tsx
+++ client/src/pages/list.tsx
@@ -2,12 +2,20 @@ import { NextPage } from 'next'
 import React from 'react'
 import Container from '../components/Container'
 import IdeaList from '../components/IdeaList'
+import Link from 'next/link'
 
 export const List: NextPage = () => {
   return (
     <Container maxWidth='sm'>
-      <h1 className='font-bold text-2xl'>Ideas</h1>
-      <IdeaList />
+      <div className='flex flex-col'>
+        <h1 className='font-bold text-2xl'>Ideas</h1>
+        <IdeaList />
+        <Link href='/'>
+          <a className='text-link-100 text-center w-full px-8 py-4 rounded uppercase font-bold bg-link-500'>
+            add more
+          </a>
+        </Link>
+      </div>
     </Container>
   )
 }
