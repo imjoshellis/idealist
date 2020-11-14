827f253f13717408b53aca16d6db3921007aa95b
Josh Ellis <josh@imjoshellis.com>
2020-11-14 12:17:32 -0600

    Add max width prop

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
-interface ContainerProps {}
+interface ContainerProps {
+  maxWidth?: 'none' | 'sm' | 'md' | 'lg'
+}
 
-export const Container: React.FC<ContainerProps> = ({ children }) => {
+export const Container: React.FC<ContainerProps> = ({
+  children,
+  maxWidth = 'none'
+}) => {
   return (
     <div
       data-testid='container'
-      className='flex justify-center items-center h-screen w-screen'
+      className='flex justify-center items-center h-screen w-screen px-4 md: px-8 lg:px-24'
     >
-      <div className=''>{children}</div>
+      <div className={`w-full max-w-${maxWidth}`}>{children}</div>
+      <div className='max-w-sm max-w-none max-w-lg max-w-md'></div>
     </div>
   )
 }
