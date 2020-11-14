bb70fcf26a7d1095b4e2dd8b614b15d1c346b636
Josh Ellis <josh@imjoshellis.com>
2020-11-14 12:18:47 -0600

    Add styles

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,10 +5,16 @@ import Container from '../components/Container'
 const Home = () => {
   return (
     <Container>
-      <IdeaSubmission />
-      <Link href='/list'>
-        <a>done</a>
-      </Link>
+      <div className='flex flex-row w-full'>
+        <div className='flex-grow mr-4'>
+          <IdeaSubmission />
+        </div>
+        <Link href='/list'>
+          <a className='text-link-100 px-8 py-4 rounded uppercase font-bold bg-link-500'>
+            done ->
+          </a>
+        </Link>
+      </div>
     </Container>
   )
 }
