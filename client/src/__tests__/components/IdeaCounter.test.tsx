bcbaded7e7420078408b131b70690439c554bb90
Josh Ellis <josh@imjoshellis.com>
2020-11-14 13:49:12 -0600

    Add goal logic to counter

--- client/src/__tests__/components/IdeaCounter.test.tsx
+++ client/src/__tests__/components/IdeaCounter.test.tsx
@@ -4,12 +4,28 @@ import IdeaCounter from '../../components/IdeaCounter'
 import faker from 'faker'
 
 describe('IdeaCounter', () => {
-  it('renders a list from context', () => {
+  it('Shows a dynamic count of ideas', () => {
     let ideas = [] as string[]
     for (let i = 0; i < 3; i++) {
       ideas.push(faker.lorem.sentence())
     }
     render(<IdeaCounter />, { ideas })
     expect(screen.getByTestId('idea-counter').innerHTML).toContain('3')
+  })
+
+  it('Shows a prompt if ideas is empty', () => {
+    render(<IdeaCounter />)
+    expect(screen.getByTestId('idea-counter').innerHTML).toContain(
+      'Add an idea to get started'
+    )
+  })
+
+  it('Accepts a goal prop', () => {
+    let ideas = [] as string[]
+    for (let i = 0; i < 3; i++) {
+      ideas.push(faker.lorem.sentence())
+    }
+    render(<IdeaCounter goal={20} />, { ideas })
+    expect(screen.getByTestId('idea-counter').innerHTML).toContain('20')
   })
 })
--- client/src/components/IdeaCounter.tsx
+++ client/src/components/IdeaCounter.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { useIdeas } from '../providers/IdeaProvider'
 
-interface IdeaCounterProps {}
+interface IdeaCounterProps {
+  goal?: number
+}
 
-export const IdeaCounter: React.FC<IdeaCounterProps> = () => {
+export const IdeaCounter: React.FC<IdeaCounterProps> = ({ goal = 10 }) => {
   const [ideas] = useIdeas()
+  const count = ideas.length
+  const getString = () => {
+    if (count === 0) return 'Add an idea to get started'
+    const baseString = `Added ${count}/${goal} idea${count === 1 ? '' : 's'}`
+    if (count >= goal * 5) return `${baseString} 🦄️`
+    if (count >= goal * 2) return `${baseString} 🔥️`
+    if (count >= goal) return `${baseString} 🎉️`
+    if (count >= goal / 2) return `${baseString} 👀️`
+    return `${baseString} ✍️`
+  }
   return (
-    <span data-testid='idea-counter'>
-      You have {ideas.length} idea{ideas.length === 1 ? '' : 's'}
+    <span data-testid='idea-counter' className='text-3xl'>
+      {getString()}
     </span>
   )
 }
