02e049598964ac59a28b68885491951b6d982ec2
Josh Ellis <josh@imjoshellis.com>
2020-11-14 10:15:39 -0600

    Add ideaReducer

--- client/src/__tests__/IdeaSubmission.test.tsx
+++ client/src/__tests__/IdeaSubmission.test.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import { render, screen } from '@testing-library/react'
 import IdeaSubmission from '../components/IdeaSubmission'
+import { ideaActionTypes, ideaReducer } from '../reducers/ideaReducer'
+import userEvent from '@testing-library/user-event'
 
-describe('Idea Submission', () => {
+jest.mock('../reducers/ideaReducer')
+
+describe('IdeaSubmission', () => {
   it('contains IdeaForm', () => {
     const { container } = render(<IdeaSubmission />)
     expect(container.firstChild?.nodeName.toLowerCase()).toBe('form')
@@ -19,4 +23,20 @@ describe('Idea Submission', () => {
     expect(screen.getByRole('button', { name: /done/i })).toBeInTheDocument()
   })
 
+  it('uses ideaReducer', () => {
+    render(<IdeaSubmission />)
+    const idea = 'idea'
+
+    userEvent.type(screen.getByLabelText(/idea/i), idea)
+    userEvent.click(screen.getByRole('button', { name: /add/i }))
+    expect(ideaReducer).toHaveBeenCalled()
+
+    const action = {
+      type: ideaActionTypes.ADD_IDEA,
+      payload: idea
+    }
+    expect(ideaReducer).toHaveBeenCalledWith([], action)
+  })
+
+  it.todo('updates ideas context')
 })
--- client/src/components/IdeaSubmission.tsx
+++ client/src/components/IdeaSubmission.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ideaActionTypes, ideaReducer } from '../reducers/ideaReducer'
 import IdeaForm, { IdeaFormData } from './IdeaForm'
 
 interface IdeaSubmissionProps {}
 
 export const IdeaSubmission: React.FC<IdeaSubmissionProps> = () => {
-  const onSubmit = (data: IdeaFormData) => null
+  const onSubmit = (data: IdeaFormData) => {
+    const state = [] as string[]
+    const action = {
+      type: ideaActionTypes.ADD_IDEA,
+      payload: data.idea
+    }
+    ideaReducer(state, action)
+  }
   return (
     <>
       <IdeaForm onSubmit={onSubmit} />
