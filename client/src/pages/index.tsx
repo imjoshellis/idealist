75fa5a4e6bfee87f4306a35c6f1c2adaa042491f
Josh Ellis <josh@imjoshellis.com>
2020-11-14 11:54:16 -0600

    Wrap with container

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import IdeaSubmission from '../components/IdeaSubmission'
 import Link from 'next/link'
+import Container from '../components/Container'
 
 const Home = () => {
   return (
-    <>
+    <Container>
       <IdeaSubmission />
       <Link href='/list'>
         <a>done</a>
       </Link>
-    </>
+    </Container>
   )
 }
 
--- client/src/pages/list.tsx
+++ client/src/pages/list.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from 'next'
 import React from 'react'
+import Container from '../components/Container'
 import IdeaList from '../components/IdeaList'
 
 export const List: NextPage = () => {
   return (
-    <>
+    <Container>
       <IdeaList />
-    </>
+    </Container>
   )
 }
 
