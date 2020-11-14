1b64db8adce19aeb90ecc936fb875dba76c4d97f
Josh Ellis <josh@imjoshellis.com>
2020-11-14 11:54:24 -0600

    Add flex to container

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 interface ContainerProps {}
 
 export const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div data-testid='container'>{children}</div>
+  return (
+    <div
+      data-testid='container'
+      className='flex justify-center items-center h-screen w-screen'
+    >
+      <div className=''>{children}</div>
+    </div>
+  )
 }
 
 export default Container
