da8804dccfea8ae602f1d60f0dfe4cd977f6220c
Josh Ellis <josh@imjoshellis.com>
2020-11-15 09:57:10 -0600

    Add valid text check

--- ideas/src/idea/idea.spec.ts
+++ ideas/src/idea/idea.spec.ts
@@ -3,6 +3,11 @@ import { makeIdea } from '.'
 describe('idea', () => {
   it('can set and get text', () => {
     const text = 'idea'
-    expect(makeIdea({ text }).getText()).toEqual(text)
+    expect(makeIdea({ text }).getText()).toBe(text)
+  })
+
+  it('must have valid text', () => {
+    const invalidText = ''
+    expect(() => makeIdea({ text: invalidText })).toThrow()
   })
 })
--- ideas/src/idea/idea.ts
+++ ideas/src/idea/idea.ts
@@ -3,5 +3,8 @@ interface MakeIdeaProps {
 }
 
 export const buildMakeIdea = () => {
-  return ({ text }: MakeIdeaProps) => Object.freeze({ getText: () => text })
+  return ({ text }: MakeIdeaProps) => {
+    if (!text || text.length < 2) throw new Error('Idea must have valid text')
+    return Object.freeze({ getText: () => text })
+  }
 }
