6ae322fe1864b9f4796b5f4d1ed1f8ed05156d86
Josh Ellis <josh@imjoshellis.com>
2020-11-15 13:38:33 -0600

    Remove mongoose

--- ideas/src/idea/index.ts
+++ ideas/src/idea/index.ts
@@ -1,11 +1,11 @@
 import { buildMakeIdea } from './idea'
 import sanitizeHtml from 'sanitize-html'
-import mongoose from 'mongoose'
+import { ObjectId } from 'mongodb'
 
 const sanitize = (text: string) => sanitizeHtml(text)
 export const Id = Object.freeze({
-  makeId: () => mongoose.Types.ObjectId().toHexString(),
-  isValid: (id: string) => mongoose.Types.ObjectId.isValid(id)
+  makeId: () => new ObjectId().toHexString(),
+  isValid: (id: string) => ObjectId.isValid(id)
 })
 
 export const makeIdea = buildMakeIdea({ sanitize, Id })
--- ideas/src/test/test-utils.ts
+++ ideas/src/test/test-utils.ts
@@ -1,10 +1,10 @@
+import { ObjectId } from 'mongodb'
 import faker from 'faker'
-import mongoose from 'mongoose'
 
 export const generateMakeIdeaProps = (overrides?: any) => {
   const idea = {
     text: faker.lorem.sentence(),
-    id: mongoose.Types.ObjectId().toHexString()
+    id: new ObjectId().toHexString()
   }
 
   return { ...idea, ...overrides }
