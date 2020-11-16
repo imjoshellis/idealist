5f96e49f13a94a43735dcd3017ce92771d0d0435
Josh Ellis <josh@imjoshellis.com>
2020-11-16 12:18:41 -0600

    Refactor score

--- ideas/src/app/addScore/addScore.ts
+++ ideas/src/app/addScore/addScore.ts
@@ -1,3 +1,4 @@
+import { InsertedIdea } from './../createIdea/createIdea'
 import { makeIdea, makeScore } from '../core/entities'
 import { MakeScoreProps, ScoreNames } from '../core/types'
 
@@ -11,49 +12,42 @@ export const makeAddScore = ({
   ideaDb
 }: {
   ideaDb: {
-    findOne: ({ id }: { id: string }) => any
-    update: (obj: any) => any
+    findOne: ({ id }: { id: string }) => InsertedIdea
+    update: (obj: any) => InsertedIdea
   }
 }) => {
   return async ({ id, type, userId }: AddScoreProps) => {
     const ideaFromDb = await ideaDb.findOne({ id })
-    let makeScoreProps: MakeScoreProps = {}
-    for (const scoreType of Object.values(ScoreNames)) {
-      if (type === scoreType) {
-        makeScoreProps[scoreType] = [
-          ...ideaFromDb.score[scoreType].userIds,
-          userId
-        ]
-      } else {
-        makeScoreProps[scoreType] = [...ideaFromDb.score[scoreType].userIds]
-      }
+
+    const makeScoreProps: MakeScoreProps = {}
+    for (const [key, value] of Object.entries(ideaFromDb.score)) {
+      makeScoreProps[key as ScoreNames] =
+        key === type ? [...value.userIds, userId] : [...value.userIds]
     }
-    const score = makeScore(makeScoreProps)
+
+    const scoreEntity = makeScore(makeScoreProps)
 
     const idea = makeIdea({
       text: ideaFromDb.text,
       id: ideaFromDb.id,
       userId: ideaFromDb.userId,
-      score
+      score: scoreEntity
     })
 
-    const { STARS, LIKES, REJECTS } = ScoreNames
-    return ideaDb.update({
-      id,
-      score: {
-        stars: {
-          userIds: idea.score().getUserIds(STARS),
-          value: idea.score().getValue(STARS)
-        },
-        likes: {
-          userIds: idea.score().getUserIds(LIKES),
-          value: idea.score().getValue(LIKES)
-        },
-        rejects: {
-          userIds: idea.score().getUserIds(REJECTS),
-          value: idea.score().getValue(REJECTS)
-        }
+    const score: {
+      [key: string]: {
+        userIds: string[]
+        value: number
       }
-    })
+    } = {}
+
+    for (const type of Object.values(ScoreNames)) {
+      score[type] = {
+        userIds: idea.score().getUserIds(type),
+        value: idea.score().getValue(type)
+      }
+    }
+
+    return ideaDb.update({ id, score })
   }
 }
