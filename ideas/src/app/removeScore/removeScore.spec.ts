e7960ab790f4afcb3140635681161f4f2cdc6aea
Josh Ellis <josh@imjoshellis.com>
2020-11-16 12:42:43 -0600

    Setup remove score

--- ideas/src/app/removeScore/removeScore.spec.ts
+++ ideas/src/app/removeScore/removeScore.spec.ts
@@ -1,8 +1,58 @@
+import { generateMakeIdeaProps } from '../../__test__'
+import { ScoreNames } from '../core/types'
+import { makeCreateIdea } from '../createIdea/createIdea'
+import { makeAddScore } from './../addScore/addScore'
 import { makeRemoveScore } from './removeScore'
 
 describe('remove score', () => {
-  it('does nothing', () => {
-    const removeScore = makeRemoveScore()
-    expect(() => removeScore()).not.toThrow()
+  let ideas: any[] = []
+  const ideaDb = {
+    insert: (obj: any) => {
+      ideas.push(obj)
+      return obj
+    },
+    findOne: ({ id }: { id: string }) => ideas.filter(i => i.id === id)[0],
+    update: (obj: any) => {
+      const updatedIdea = Object.assign(
+        {},
+        ideas.filter(i => i.id === obj.id)[0],
+        obj
+      )
+      ideas = ideas.map(i => (i.id === updatedIdea.id ? updatedIdea : i))
+      return updatedIdea
+    }
+  }
+
+  let id: string
+  const userId = 'user'
+
+  beforeEach(async () => {
+    const createIdea = makeCreateIdea({ ideaDb })
+    id = (await createIdea(generateMakeIdeaProps())).id
+    const addScore = makeAddScore({ ideaDb })
+    for (const type of Object.values(ScoreNames)) {
+      addScore({ id, userId, type })
+    }
+  })
+
+  it('can remove score on an idea', async () => {
+    const removeScore = makeRemoveScore({ ideaDb })
+    for (const type of Object.values(ScoreNames)) {
+      const updatedIdea = await removeScore({ id, userId, type })
+      const { userIds, value } = updatedIdea.score[type]
+      expect(userIds).toEqual([])
+      expect(value).toEqual(0)
+    }
+  })
+
+  it('throws if idea does not exist', async () => {
+    const removeScore = makeRemoveScore({ ideaDb })
+    const type = ScoreNames.STARS
+    try {
+      await removeScore({ id: 'abc', userId, type })
+      fail('Add score should have thrown an error')
+    } catch (e) {
+      expect(e.message).toBe('Idea not found')
+    }
   })
 })
--- ideas/src/app/removeScore/removeScore.ts
+++ ideas/src/app/removeScore/removeScore.ts
@@ -1,3 +1,56 @@
-export const makeRemoveScore = () => {
-  return () => null
+import { makeIdea, makeScore } from '../core/entities'
+import { MakeScoreProps, ScoreNames } from '../core/types'
+import { InsertedIdea } from '../createIdea/createIdea'
+
+interface RemoveScoreProps {
+  id: string
+  userId: string
+  type: ScoreNames
+}
+
+export const makeRemoveScore = ({
+  ideaDb
+}: {
+  ideaDb: {
+    findOne: ({ id }: { id: string }) => Promise<InsertedIdea>
+    update: (obj: any) => Promise<InsertedIdea>
+  }
+}) => {
+  return async ({ id, userId, type }: RemoveScoreProps) => {
+    const ideaFromDb = await ideaDb.findOne({ id })
+    if (!ideaFromDb) throw new Error('Idea not found')
+
+    const makeScoreProps: MakeScoreProps = {}
+    for (const [key, value] of Object.entries(ideaFromDb.score)) {
+      makeScoreProps[key as ScoreNames] =
+        key === type
+          ? value.userIds.filter(u => u !== userId)
+          : [...value.userIds]
+    }
+
+    const scoreEntity = makeScore(makeScoreProps)
+
+    const idea = makeIdea({
+      text: ideaFromDb.text,
+      id: ideaFromDb.id,
+      userId: ideaFromDb.userId,
+      score: scoreEntity
+    })
+
+    const score: {
+      [key: string]: {
+        userIds: string[]
+        value: number
+      }
+    } = {}
+
+    for (const type of Object.values(ScoreNames)) {
+      score[type] = {
+        userIds: idea.score().getUserIds(type),
+        value: idea.score().getValue(type)
+      }
+    }
+
+    return ideaDb.update({ id, score })
+  }
 }
